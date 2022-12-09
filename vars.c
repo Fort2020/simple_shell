b39c793217dcb4caa19283afc68d06b5953afc66
fort2020 <frankfortdata@gmail.com>
2022-12-09 05:27:55 -0800

    Task vars.c done

--- vars.c
+++ vars.c
@@ -36,7 +36,6 @@ int is_chain(info_t *info, char *buf, size_t *p)
 }
 
 /**
-
  * check_chain - checks we should continue chaining based on last status
  * @info: the parameter struct
  * @buf: the char buffer
